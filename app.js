e3c705e2b881c14c0cf9a41b3ef5d362f64a5b9a
Si Ying <leowsiying@gmail.com>
2024-06-29 22:49:15 +0800

    added the GUI and link the HTML to the JavaScript

--- app.js
+++ app.js
@@ -1,63 +1,74 @@
-function playGame() {
+document.addEventListener("DOMContentLoaded", function () {
   let humanScore = 0;
   let computerScore = 0;
   const endScore = 5;
 
+  function playWinAudio() {
+    const winAudio = document.getElementById("win_audio");
+    winAudio.play();
+  }
+
+  function playLoseAudio() {
+    const loseAudio = document.getElementById("lose_audio");
+    loseAudio.play();
+  }
+
   function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let randomNum = Math.floor(Math.random() * 3);
-    console.log("Random number: " + randomNum);
     return options[randomNum];
   }
 
-  function getHumanChoice() {
-    var readline = require("readline-sync");
-    let option;
-    let validChoices = ["rock", "paper", "scissors"];
-
-    while (true) {
-        option = readline.question("Please select rock, paper or scissors: ").toLowerCase();
-        if (validChoices.includes(option)) {
-            break;
-        } else {
-            console.log("Invalid choice, please select rock, paper or scissors.");
-        }
-    }
-    return option;
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+  function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
 
     if (humanChoice === computerChoice) {
-      console.log("It's a tie! Both chose " + humanChoice);
+      alert("It's a tie! Both chose " + humanChoice);
     } else if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "paper" && computerChoice === "rock") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
       humanScore++;
-      console.log("You win! " + humanChoice + " beats " + computerChoice);
+      document.getElementById("playerScore").innerText = "Score: " + humanScore;
+      alert("You win! " + humanChoice + " beats " + computerChoice);
     } else {
       computerScore++;
-      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      document.getElementById("computerScore").innerText = "Score: " + computerScore;
+      alert("You lose! " + computerChoice + " beats " + humanChoice);
     }
-    console.log(
-      "Scores: Human: " + humanScore + ", Computer: " + computerScore
-    );
+
+    if (humanScore === endScore) {
+      playWinAudio();
+      setTimeout(function () {
+        alert("You win the game!");
+        resetGame();
+      }, 1000);
+    } else if (computerScore === endScore) {
+      playLoseAudio();
+      setTimeout(function () {
+        alert("The computer wins the game!");
+        resetGame();
+      }, 1000);
+    }
   }
 
-  while (humanScore < endScore && computerScore < endScore) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+  function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById("playerScore").innerText = "Score: 0";
+    document.getElementById("computerScore").innerText = "Score: 0";
   }
 
-  if (humanScore === endScore) {
-    console.log("You win the game!");
-  } else if (computerScore === endScore) {
-    console.log("The computer wins the game!")
-  }
-}
+  document.getElementById("rockdiv").addEventListener("click", function () {
+    playRound("rock");
+  });
 
-playGame();
+  document.getElementById("paperdiv").addEventListener("click", function () {
+    playRound("paper");
+  });
+
+  document.getElementById("scissorsdiv").addEventListener("click", function () {
+    playRound("scissors");
+  });
+});
